4e468680cbca7e9a63a9979dbe927c1d6ecc7711
arthur albuquerque <arthuralbuquerque.hsh@gmail.com>
2024-12-19 16:14:11 -0300

    Array

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+namespace Array{
+    class Program{
+        static void Main(string[]args){           
+            Console.Clear();
+            var meuArray = new int[5];
+            meuArray[0]=12;
+            meuArray[1]=12;
+            meuArray[2]=10;
+            meuArray[3]=11;
+            Console.WriteLine(meuArray[0]);
+            Console.WriteLine(meuArray[1]);
+            Console.WriteLine(meuArray[2]);
+            Console.WriteLine(meuArray[3]);
+            var Array = new int[5]{10,20,30,40,50};
+            Console.WriteLine(Array[0]);
+            Console.WriteLine(Array[1]);
+            Console.WriteLine(Array[2]);
+            Console.WriteLine(Array[3]);
+            Console.WriteLine(Array[4]);
+            
+            
+           
+
+        }
+    }
+}
